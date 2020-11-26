c8b0f2c0d69649295cbd63c2a96a9d4ef168e659
Aris Georgoulas <aris.georgoulas@hotmail.com>
2020-11-26 22:36:23 +0200

    initial_cfip_commit

---
+++ exercises/Country_from_IP.py
@@ -0,0 +1,18 @@
+#! python3
+
+import re
+import json
+from urllib.request import urlopen  #use urllib.request for python3, or urlib2 for python2
+
+url = 'http://ipinfo.io/json'
+response = urlopen(url)
+data = json.load(response)
+
+IP=data['ip']
+org=data['org']
+city = data['city']
+country=data['country']
+region=data['region']
+
+print('Your IP detail\n ')
+print('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
