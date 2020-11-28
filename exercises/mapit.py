a2b6050518e18a037e2b8c4e19742f44c760d81c
Aris Georgoulas <aris.georgoulas@hotmail.com>
2020-11-28 11:03:50 +0200

    added comments

--- exercises/mapit.py
+++ exercises/mapit.py
@@ -3,7 +3,8 @@
 #I want to run it from the command line directly so I 'll create a batch file too.
 import sys, webbrowser, pyperclip
 
-sys.argv # ['mapit.py', '870', 'Valencia', 'St.']
+sys.argv # ['mapit.py', '870', 'Valencia', 'St.'] 
+# we want all the system arguments after the first one which is the executable
 
 #check if command line arguments were passed
 if len(sys.argv) > 1:
