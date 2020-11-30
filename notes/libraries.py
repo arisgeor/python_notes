6f0a93790f65da87e9fdcaa321d9d81cb8524304
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-11-30 20:35:32 +0200

    libraries_notes.py initial commit

---
+++ notes/libraries.py
@@ -0,0 +1,17 @@
+#! python3
+
+#A list of all the libraries I' ve used.
+#those that need to be istalled have a comment next to them.
+
+import pprint
+import os
+import shelve
+import re
+import pyperclip
+import PyPDF2 #pip install PyPDF2
+import docx #pip install python-docx
+import openpyxl #pip install openpyxl 
+import webbrowser
+import requests #pip install requests
+import bs4 #pip install beautifulsoup4
+from selenium import webdriver #pip install selenium
