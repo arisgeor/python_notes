f759f45b5204855713566bc099001ea61d307436
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-03 21:15:36 +0200

    added libraries

--- notes/libraries_notes.py
+++ notes/libraries_notes.py
@@ -3,16 +3,26 @@
 #A list of all the libraries I' ve used.
 #those that need to be istalled have a comment next to them.
 
+#Standard Library
+import datetime
+import json
+import tkinter
 import pprint
 import os
 import shelve
 import re
-import pyperclip
-import PyPDF2 #pip install PyPDF2
-import docx #pip install python-docx
-import openpyxl #pip install openpyxl 
 import webbrowser
-import requests #pip install requests
-import bs4 #pip install beautifulsoup4
+import pyperclip
+
+#
+import PyPDF2               #pip install PyPDF2
+import docx                 #pip install python-docx
+import openpyxl             #pip install openpyxl 
+import requests             #pip install requests
+import bs4                  #pip install beautifulsoup4
 from selenium import webdriver #pip install selenium
-import rich #pip install rich
+import rich                 #pip install rich
+import PIL                  #pip install Pillow 
+import numpy                #pip install numpy
+import cv2                  #pip install opencv-python
+import pygame               #pip install -U pygame --user #python3 -m pygame.examples.aliens to check if it worked :P
