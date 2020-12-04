20329a90a45322a638039847d1fe32a1590e55a6
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-04 14:46:46 +0200

    email_notes_init

---
+++ notes/email_notes.py
@@ -0,0 +1,16 @@
+#! python3
+
+#Simple Mail Transfer Protocol
+import smtplib 
+conn = smtplib.SMTP('smtp.gmail.com', 587) #server, port_number
+conn.starttls() #begin the encryption #220 means everything is ok.
+conn.ehlo() #connect to smtp server.
+
+conn.login('aoristos13@gmail.com', 'kadcaihaecagsaxmaloargaa')
+conn.sendmail('aoristos13@gmail.com', 'aris.georgoulas@hotmail.com','Subject: So long \n\n Dear Aris\n, Its been so long since I last heard from you')
+#the structure is:(from, to, body)
+conn.quit()
+
+#Gmail --> smtp.gmail.com, 587
+#Outlook --> smtp-mail.outlook.com, 587
+#Yahoo --> smtp.mail.yahoo.com, 587
