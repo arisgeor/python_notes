1fabcced4bb44368a0e6e64851b983f20bf6f9c8
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-09 10:25:00 +0200

    added_comments

--- notes/hello.py
+++ notes/hello.py
@@ -2,10 +2,10 @@
 
 print("Hello World!")
 
-
-# batch files just combine multiple files as one executable.
-# I can use the run prompt to run batch files directly by typing 
+# This goes hand in hand with the 'hello.bat' file, to demostrate its use. 
+# Batch files just combine multiple files as one executable.
+# I can use the run prompt (Win+R) to run batch files directly by typing 
 # just the name of the .bat file, provided its folder is in the 
 # path system variable. 
-# If i want to run "hello.bat" i need to add C:\Python39\Python_Code\python_notes\notes\ 
+# If I want to run "hello.bat", I need to add C:\Python39\Python_Code\python_notes\notes\ 
 # to the "path" system variable since hello.bat is located in that folder! 
