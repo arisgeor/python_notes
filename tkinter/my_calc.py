46d64036e081659434f45f83f0efef80cd9b9721
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-10 14:18:45 +0200

    added comments

--- tkinter/my_calc.py
+++ tkinter/my_calc.py
@@ -11,7 +11,7 @@ e.grid(row=0, column=0, columnspan = 3, padx = 10, pady = 10) #columnspan is exa
 ###functions.
 
 def button_click(number):
-    #e.delete(0, END) #if i delete the value then i ll never be able to type a number like 108 for example. Also if i want to type 108 it will print 801
+    #e.delete(0, END) #if I delete the value then I 'll never be able to type a number like 108 for example. Also if I want to type 108 it will print 801
     current = e.get() #that's why I will first get the value inside the box and concatinate it with the next value. So after I get the first value:
     e.delete(0,END)
     e.insert(0, str(current) + str(number)) #to add the 'current' with the number you click, you need to make sure there are strings! otherwise it will add the numeric values.
@@ -66,22 +66,21 @@ def button_equal():
 
 ###Buttons
 
-#instead of passing a command parameter, I actually want to call it with a specific argument to each button, thats why i use lambda without any arguments! 
+# Instead of passing a command parameter, I actually want to call it with a specific argument to each button, 
+# thats why I use lambda without any arguments! 
 Button(root,text='7', padx=40, pady=20, command=lambda: button_click(7)).grid(row = 1, column = 0)
 Button(root,text='8', padx=40, pady=20, command=lambda: button_click(8)).grid(row = 1, column = 1)
 Button(root,text='9', padx=40, pady=20, command=lambda: button_click(9)).grid(row = 1, column = 2)
-
 Button(root,text='4', padx=40, pady=20, command=lambda: button_click(4)).grid(row = 2, column = 0)
 Button(root,text='5', padx=40, pady=20, command=lambda: button_click(5)).grid(row = 2, column = 1)
 Button(root,text='6', padx=40, pady=20, command=lambda: button_click(6)).grid(row = 2, column = 2)
-
 Button(root,text='1', padx=40, pady=20, command=lambda: button_click(1)).grid(row = 3, column = 0)
 Button(root,text='2', padx=40, pady=20, command=lambda: button_click(2)).grid(row = 3, column = 1)
 Button(root,text='3', padx=40, pady=20, command=lambda: button_click(3)).grid(row = 3, column = 2)
-
 Button(root,text='0', padx=40, pady=20, command=lambda: button_click(0)).grid(row = 4, column = 0)
 
-Button(root,text='Clear', padx=79, pady=20, command=button_clear).grid(row = 4, column = 1, columnspan = 2) #ofc here I dont need lambda.
+#For the other Buttons, I have made different methods, so no need for lambda, just call the method.
+Button(root,text='Clear', padx=79, pady=20, command=button_clear).grid(row = 4, column = 1, columnspan = 2) 
 Button(root,text='+', padx=39, pady=20, command=button_add).grid(row = 5, column = 0)
 Button(root,text='=', padx=91, pady=20, command=button_equal).grid(row = 5, column = 1, columnspan = 2)
 
