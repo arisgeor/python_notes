8336030cf95cb8aa9c9ec5415c806e770c3a187e
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-11 20:34:37 +0200

    dataset_import

--- deep_learning/ann/101.py
+++ deep_learning/ann/101.py
@@ -1,2 +1,11 @@
 #! python3 
 
+import numpy as np
+import pandas as pd
+import tensorflow as tf #tf currently not working with Python 3.9!!!
+
+tf.__version__ #check your version!
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values                    #grab all columns from the 3rd to the last (without including it!)
+y = dataset.iloc[:, -1].values                      #grab the last column
