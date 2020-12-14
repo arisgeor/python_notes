7211c5098609b7a5d7a763925574a7a04e36613f
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-14 21:59:00 +0200

    message_boxes_init

---
+++ tkinter/tk_message_boxes.py
@@ -0,0 +1,12 @@
+#! python3 
+
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+
+root = Tk()
+root.title('Message Boxes')
+root.iconbitmapr('C:\Users\Aristos\Desktop\Aristos\random_pics\pacman.ico')
+
+
+root.mainloop()
