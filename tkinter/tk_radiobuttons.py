6029c7ebb106f8136facb8d2f0b87911456a4786
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-14 21:55:17 +0200

    updated the pizza example

--- tkinter/tk_radiobuttons.py
+++ tkinter/tk_radiobuttons.py
@@ -4,7 +4,29 @@ from PIL import ImageTk, Image
 
 root = Tk() #our widget
 root.title('Radio Buttons!')
-root.iconbitmap(r'C:\Users\Aristos\Desktop\Aristos\random_pics\pacman.ico').
+root.iconbitmap(r'C:\Users\Aristos\Desktop\Aristos\random_pics\pacman.ico')
 
+#r = IntVar() #declare it, so it can be tracked.
+#r.set('2') #initialize it
+
+PIZZA_LIST = [
+    ('Pizza 1', 'Pepperoni'),
+    ('Pizza 2', 'Cheese'),
+    ('Pizza 3', 'Mushrooms'),
+    ('Pizza 4', 'Greek'),
+]
+
+pizza = StringVar() #track the variable of the radio buttons!
+pizza.set("Pepperoni") #set the 1st.
+
+for pizza_num, flavor in PIZZA_LIST:
+    Radiobutton(root, text = pizza_num + ' : ' + flavor, variable = pizza, value = flavor).pack(anchor=W)
+
+def clicked(value):
+    myLabel1 = Label(root, text = value)
+    myLabel1.pack()
+
+myButton = Button(root, text = 'Click Me!', command = lambda: clicked(pizza.get()))
+myButton.pack()
 
 root.mainloop()
