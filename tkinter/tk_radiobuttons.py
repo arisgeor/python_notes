6c1d55e9583684a9fda5ec9e971263bcd4ffeaf7
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-14 21:33:54 +0200

    radio_init

---
+++ tkinter/tk_radiobuttons.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk() #our widget
+root.title('Radio Buttons!')
+root.iconbitmap(r'C:\Users\Aristos\Desktop\Aristos\random_pics\pacman.ico').
+
+
+root.mainloop()
