e1bdb87f29d26c869d255c39a85e4bcf53891bd9
arisgeor <30298541+arisgeor@users.noreply.github.com>
2020-12-28 18:48:46 +0200

    1_complete

--- OpenCV/1_reading_images_and_video.py
+++ OpenCV/1_reading_images_and_video.py
@@ -1,6 +1,27 @@
 import cv2 as cv
 
+###Images###
+
 img = cv.imread('Photos/cat.jpg')
 cv.imshow('Cat', img)               #displayes an image (the argument img)
+
 cv.waitKey(0)                       #waits for an inf amount of time
+
+###Videos###
+
+#capture = cv.VideoCapture(0) #0 references the webcam
+#now we ll examine an already existing video, so,
+capture = cv.VideoCapture('Videos/flash_over_q.webm')
+while True:
+    isTrue, frame = capture.read()  #reads the specified video, frame by frame.
+    cv.imshow('Video', frame)       #so its like projecting all images that compose the video.
+
+    if cv.waitKey(33) & 0xFF==ord('q'): #closes when you press 'q', #waitkey controlls the playback speed of the video! 33seems about right.
+        break
+
+#Error -215:Assertion failed is when the video runs out of frames to show!
+
+capture.release()
+cv.destroyAllWindows()
+
 
