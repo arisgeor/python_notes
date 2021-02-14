1e7b044b91f2d164eecf9a94fce4a6d5c57864c8
arisgeor <30298541+arisgeor@users.noreply.github.com>
2021-02-14 14:49:21 +0200

    __unfinished__

---
+++ OpenCV/4_basic_functions.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+
+img = cv.imread('Photos/cat.jpg')
+cv.imshow('Cat', img)               #displayes an image (the argument img)
+
+cv.waitKey(0)                       #waits for an inf amount of time
